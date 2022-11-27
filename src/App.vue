df2acbba28b35deb390086495f084bf82e0e4859
KeisukeKuwahara <kuwa.90k@gmail.com>
2022-11-27 11:13:36 +0900

    タイトルを追加

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,18 @@ import MsGame from "./components/MsGame.vue";
 </script>
 
 <template>
+  <h1 class="title">Vue-Minesweeper</h1>
   <div class="content">
     <MsGame />
   </div>
 </template>
 
 <style scoped>
+.title {
+  height: 50px;
+  color: rgb(64, 215, 59);
+  text-align: center;
+}
 .content {
   display: flex;
   justify-content: center;
